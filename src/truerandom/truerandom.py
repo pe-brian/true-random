823fbf729032bb2ac061d958ca60abffdbfa4451
Pierre-Emmanuel Brian <pebrian@outlook.fr>
2022-04-13 22:49:58 +0200

    Input params type checking

--- src/truerandom/truerandom.py
+++ src/truerandom/truerandom.py
@@ -16,6 +16,9 @@ def true_randint(
     """
         Returns a random integer between <ge> and <le>
     """
+    if not isinstance(ge, int) or not isinstance(le, int):
+        raise TypeError("ge and le must be integers")
+
     return int(qtr.randint(min=ge, max=le, generator=GENERATOR))
 
 
@@ -25,6 +28,10 @@ def true_choice(
     """
         Returns a random item from the list <x>
     """
+    if not isinstance(x, List) and not isinstance(x, Tuple) \
+            and not isinstance(x, str):
+        raise TypeError(f"x must be a list or a tuple (instead of {type(x)})")
+
     return x[true_randint(0, len(x) - 1)]
 
 
@@ -34,6 +41,9 @@ def true_shuffle(
     """
         Shuffles list <x> in place, and returns None.
     """
+    if not isinstance(x, List):
+        raise TypeError(f"x must be a list (instead of {type(x)})")
+
     for i in reversed(range(1, len(x))):
         j = true_randint(0, i + 1)
         x[i], x[j] = x[j], x[i]
@@ -46,17 +56,28 @@ def true_password(
     has_digits: bool = True
 ) -> str:
     """
-        Generates a true random password composed of
-        <length> character(s) composed of eventually:
-        - punctuations
-        - uppercase letters
-        - digits
-        - lowercase letters
+        Generates a random password composed of <length> character(s) with:
+        - 0 or more punctuations
+        - 0 or more uppercase letters
+        - 0 or more digits
+        - Remaining slots are filled with lowercase letters.
     """
 
+    if not isinstance(length, int):
+        raise TypeError("param 'length' must be an integer")
+
+    if not isinstance(has_punctuation, bool):
+        raise TypeError("param 'has_punctuation' must be a boolean")
+
+    if not isinstance(has_uppercase_letters, bool):
+        raise TypeError("param 'has_uppercase_letters' must be a boolean")
+
+    if not isinstance(has_digits, bool):
+        raise TypeError("param 'has_digits_letters' must be a boolean")
+
     if length < 8:
-        raise Exception(
-            "Length must be greater or equal than 8 characters")
+        raise ValueError(
+            "param 'length' must be >= 8")
 
     password = ""
 
@@ -101,8 +122,30 @@ def true_passwords(
     """
         Generate a csv file containing <nb> true passwords
     """
+    if not isinstance(nb, int):
+        raise TypeError("param 'nb' must be an integer")
+
+    if not isinstance(nb, str):
+        raise TypeError("param 'csv_output' must be a str")
+
+    if not isinstance(nb, length):
+        raise TypeError("param 'length' must be an integer")
+
+    if not isinstance(has_punctuation, bool):
+        raise TypeError("param 'has_punctuation' must be a boolean")
+
+    if not isinstance(has_uppercase_letters, bool):
+        raise TypeError("param 'has_uppercase_letters' must be a boolean")
+
+    if not isinstance(has_digits, bool):
+        raise TypeError("param 'has_digits_letters' must be a boolean")
+
     if nb <= 0:
-        raise Exception("nb param must be positive")
+        raise Exception("param 'nb' must be stricly positive")
+
+    if length < 8:
+        raise ValueError(
+            "param 'length' must be >= 8")
 
     with open(csv_output, 'w', newline='', encoding='utf8') as csvfile:
         writer = csv.writer(
